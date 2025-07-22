61085a553179aea89119e3d09483c49d4976bb96
soyeon513 <107165543+soyeon513@users.noreply.github.com>
2025-07-22 02:51:34 +0000

    Deploying to gh-pages from @ soyeon513/soyeon513.github.io@b75d20e0c2e0f1a0f807d738a979e769153c18fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-teaching",
-          title: "Teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-research",
+  },{id: "nav-research",
           title: "Research",
           description: "Materials for peer-reviewed publications. Replace this text with your description.",
           section: "Navigation",
