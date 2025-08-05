53f0cbd6cb5ee7030f0f8d0dba3495c5c63b23a8
soyeon513 <107165543+soyeon513@users.noreply.github.com>
2025-08-05 00:57:20 +0000

    Deploying to gh-pages from @ soyeon513/soyeon513.github.io@454bcca05acdbd379308ae8b417c838f2b041559 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -455,6 +455,13 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://bsky.app/profile/soyeonpark.bsky.social", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
